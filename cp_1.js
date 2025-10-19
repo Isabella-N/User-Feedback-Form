c3eeb2fdb064b5b891cc22cfa3712f11a816c966
Isabella Jones <injones@usf.edu>
2025-10-19 17:30:16 -0400

    Initial Commit Character Count added and Feedback_display added

--- cp_1.js
+++ cp_1.js
@@ -1,4 +1,10 @@
-const form = document.getElementById("feedback_form")
-const username = document.getElementById("username")
-const email = document.getElementById("email")
-const comments = document.getElementById("comments")
+const form = document.getElementById("feedback_form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const comments = document.getElementById("comments");
+const charCount = document.getElementById("char_count");
+
+// Counts the characters in real time
+comments.addEventListener("input", () => {
+  charCount.textContent = `Characters: ${comments.value.length}`;
+});
