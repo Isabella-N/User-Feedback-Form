d0d7103ec16495a6cd5ac09493ad028c6abd8e04
Isabella Jones <injones@usf.edu>
2025-10-19 15:16:46 -0400

    Initial Commit with const added onto JS

---
+++ cp_1.js
@@ -0,0 +1,4 @@
+const form = document.getElementById("feedback_form")
+const username = document.getElementById("username")
+const email = document.getElementById("email")
+const comments = document.getElementById("comments")
