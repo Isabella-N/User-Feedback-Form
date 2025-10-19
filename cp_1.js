5007aa1c9eeeabb9856dd701ac92498d2cd46ab6
Isabella Jones <injones@usf.edu>
2025-10-19 18:48:37 -0400

    Initial Commit of feedback_display with alerts created and .innerHTML added

--- cp_1.js
+++ cp_1.js
@@ -3,6 +3,7 @@ const username = document.getElementById("username")
 const email = document.getElementById("email")
 const comments = document.getElementById("comments")
 const charCount = document.getElementById("char_count")
+const feedbackDisplay = document.getElementById("feedback_display")
 
 // Counts the characters in real time
 comments.addEventListener("input", () => {
@@ -29,3 +30,29 @@ form.addEventListener("mouseout", (e) => {
   if (e.target.tagName === "INPUT" || e.target.tagName === "TEXTAREA") {
     tooltip.style.display = "none"}
 });
+
+// Display for a completed form
+form.addEventListener("submit", (e) => {
+  e.preventDefault()
+
+  // An alert is shown if all values arent filled in
+  if (!username.value.trim() || !email.value.trim() || !comments.value.trim()) {
+    alert("All fields need to be filled to be submitted")
+    return}
+
+  // The entered feedback being shown after clicking submit
+  const entry = document.createElement("div")
+  entry.classList.add("feedback_entry")
+    // Adding the feed back into the HTML
+  entry.innerHTML = `
+    <p><strong>${username.value}</strong> (${email.value}) says:</p>
+    <p>${comments.value}</p>
+    <h2> Submitted Form </h2>`
+  feedbackDisplay.appendChild(entry)
+
+  // Clearing the fields once the the submit button has been clicked
+  username.value = ""
+  email.value = ""
+  comments.value = ""
+  charCount.textContent = "Characters: 0"
+})
